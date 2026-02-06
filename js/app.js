8a7ef71455f2580cb888e5e9aef6d47b137598dc
s85920 <s85920@bht-berlin.de>
2026-02-06 09:21:35 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,49 @@
+//https://nostalgic-css.github.io/NES.css/#
+//https://fonts.google.com/selection/embed
+//
+// Button Toggles
+document.querySelectorAll(".nes-btn").forEach((button) => {
+  button.addEventListener("click", function () {
+    if (
+      this.classList.contains("is-primary") ||
+      this.classList.contains("is-warning")
+    ) {
+      this.classList.toggle("is-primary");
+      this.classList.toggle("is-warning");
+      return;
+    }
+
+    if (
+      this.classList.contains("is-success") ||
+      this.classList.contains("is-error")
+    ) {
+      this.classList.toggle("is-success");
+      this.classList.toggle("is-error");
+    }
+  });
+});
+
+// Badge Toggles
+document.querySelectorAll(".nes-badge").forEach((badge) => {
+  badge.addEventListener("click", function () {
+    const badgeLabel = this.querySelector("span");
+    if (!badgeLabel) return;
+
+    if (
+      badgeLabel.classList.contains("is-primary") ||
+      badgeLabel.classList.contains("is-warning")
+    ) {
+      badgeLabel.classList.toggle("is-primary");
+      badgeLabel.classList.toggle("is-warning");
+      return;
+    }
+
+    if (
+      badgeLabel.classList.contains("is-success") ||
+      badgeLabel.classList.contains("is-error")
+    ) {
+      badgeLabel.classList.toggle("is-success");
+      badgeLabel.classList.toggle("is-error");
+    }
+  });
+});
